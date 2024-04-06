8935d62c2a5a73a0998728aea22d18fc6ab1e244
León Orell Valerian Liehr <me@fmease.dev>
2024-04-06 23:00:33 +0200

    Refactor main a bit

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@
 use crate::utility::Tag;
 use attribute::Attributes;
 use builder::{BuildMode, QueryMode};
-use command::{CrateNameBuf, CrateNameCow, Edition};
-use std::process::ExitCode;
+use command::{CrateNameBuf, CrateNameCow, CrateType, Edition};
+use std::{path::Path, process::ExitCode};
 
 mod attribute;
 mod builder;
@@ -62,56 +62,25 @@ fn try_main() -> error::Result {
 
     // FIXME: eagerly lower `-f`s to `--cfg`s here, so we properly support them in `compiletest`+command
 
-    let build_mode = match (cross_crate, compiletest) {
-        (true, false) => BuildMode::CrossCrate,
-        (false, true) => BuildMode::Compiletest {
-            query: match (query, build_flags.json) {
-                (true, false) => Some(QueryMode::Html),
-                (true, true) => Some(QueryMode::Json),
-                (false, _) => None,
-            },
-        },
-        (false, false) => BuildMode::Default,
-        (true, true) => unreachable!(), // Already caught by `clap`.
-    };
+    let query_mode = compute_query_mode(query, build_flags.json);
+    let build_mode = compute_build_mode(cross_crate, compiletest, query_mode);
 
     let edition = edition.unwrap_or_else(|| match build_mode {
         BuildMode::Default | BuildMode::CrossCrate => Edition::LATEST_STABLE,
         BuildMode::Compiletest { .. } => Edition::default(),
     });
 
-    let source;
-    let (crate_name, crate_type) = match (crate_name, crate_type) {
-        (Some(crate_name), Some(crate_type)) => (crate_name.into(), crate_type),
-        (crate_name, crate_type) => {
-            let (crate_name, crate_type): (Option<CrateNameCow<'_>>, _) = match build_mode {
-                BuildMode::Default | BuildMode::CrossCrate => {
-                    source = std::fs::read_to_string(&path)?;
-                    // FIXME: doesn't contain `-f`s; eagerly expand them into `--cfg`s above
-                    let attributes = Attributes::parse(
-                        &source,
-                        &build_flags.cfgs,
-                        edition,
-                        program_flags.verbose,
-                    );
-
-                    let crate_name: Option<CrateNameCow<'_>> = crate_name
-                        .map(Into::into)
-                        .or_else(|| attributes.crate_name.map(Into::into));
-
-                    (crate_name, crate_type.or(attributes.crate_type))
-                }
-                BuildMode::Compiletest { .. } => (crate_name.map(Into::into), crate_type),
-            };
-
-            // FIXME: unwrap
-            let crate_name =
-                crate_name.unwrap_or_else(|| CrateNameBuf::from_path(&path).unwrap().into());
-            let crate_type = crate_type.unwrap_or_default();
-
-            (crate_name, crate_type)
-        }
-    };
+    let mut source = String::new();
+    let (crate_name, crate_type) = compute_crate_name_and_type(
+        crate_name,
+        crate_type,
+        build_mode,
+        &path,
+        edition,
+        &build_flags.cfgs,
+        &program_flags,
+        &mut source,
+    )?;
 
     let crate_name = builder::build(
         build_mode,
@@ -128,4 +97,64 @@ fn try_main() -> error::Result {
     }
 
     Ok(())
+}
+
+fn compute_query_mode(query: bool, json: bool) -> Option<QueryMode> {
+    match (query, json) {
+        (true, false) => Some(QueryMode::Html),
+        (true, true) => Some(QueryMode::Json),
+        (false, _) => None,
+    }
+}
+
+fn compute_build_mode(cross_crate: bool, compiletest: bool, query: Option<QueryMode>) -> BuildMode {
+    match (cross_crate, compiletest) {
+        (true, false) => BuildMode::CrossCrate,
+        (false, true) => BuildMode::Compiletest { query },
+        (false, false) => BuildMode::Default,
+        (true, true) => unreachable!(), // Already caught by `clap`.
+    }
+}
+
+fn compute_crate_name_and_type<'src>(
+    crate_name: Option<CrateNameBuf>,
+    crate_type: Option<CrateType>,
+    build_mode: BuildMode,
+    path: &Path,
+    edition: Edition,
+    cfgs: &[String],
+    program_flags: &cli::ProgramFlags,
+    source: &'src mut String,
+) -> error::Result<(CrateNameCow<'src>, CrateType)> {
+    Ok(match (crate_name, crate_type) {
+        (Some(crate_name), Some(crate_type)) => (crate_name.into(), crate_type),
+        (crate_name, crate_type) => {
+            let (crate_name, crate_type): (Option<CrateNameCow<'_>>, _) = match build_mode {
+                BuildMode::Default | BuildMode::CrossCrate => {
+                    *source = std::fs::read_to_string(path)?;
+                    let attributes = Attributes::parse(
+                        source,
+                        // FIXME: doesn't contain `-f`s; eagerly expand them into `--cfg`s in main
+                        cfgs,
+                        edition,
+                        program_flags.verbose,
+                    );
+
+                    let crate_name: Option<CrateNameCow<'_>> = crate_name
+                        .map(Into::into)
+                        .or_else(|| attributes.crate_name.map(Into::into));
+
+                    (crate_name, crate_type.or(attributes.crate_type))
+                }
+                BuildMode::Compiletest { .. } => (crate_name.map(Into::into), crate_type),
+            };
+
+            // FIXME: unwrap
+            let crate_name =
+                crate_name.unwrap_or_else(|| CrateNameBuf::from_path(path).unwrap().into());
+            let crate_type = crate_type.unwrap_or_default();
+
+            (crate_name, crate_type)
+        }
+    })
 }
